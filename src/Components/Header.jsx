671f62b35cfbaa912455651e54fb4c7f9c705776
techycode-01 <xp27anujkumar@gmail.com>
2024-10-24 01:27:15 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="w-full bg-blue-200 flex justify-center px-7 py-2 text-black font-semibold">
+      <div className="text-3xl">React Dropdown</div>
+    </div>
+  );
+}
+
+export default Header;
